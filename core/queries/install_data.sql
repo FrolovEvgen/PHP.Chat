2da072001cc29337ec294babfcff2d0d5a1164ec
E.Frolov <frolov@amiriset.com>
2020-02-17 21:15:31 +0200

    [L9.Part3] Add sql queries for create database? tablese and fill it with data.

---
+++ core/queries/install_data.sql
@@ -0,0 +1,62 @@
+--
+-- Запоминаем текущее время.
+--
+
+SET @TIME_NOW = UNIX_TIMESTAMP('2020-02-17 00:00:01');
+
+-- --------------------------------------------------------
+
+--
+-- Добавляем пользователей.
+--
+
+INSERT INTO `users`
+(`id`, `username`, `email`, `phone`, `icon`, `created`)
+VALUES
+(NULL, "Петя", "petya@email.tst", "36456325462", "user1.jpg", @TIME_NOW),
+(NULL, "Вася", "vasya@email.tst", "54754785549", "user2.jpg", @TIME_NOW),
+(NULL, "Федя", "fedya@email.tst", "06856898658", "user3.jpg", @TIME_NOW),
+(NULL, "Вика", "vika80@email.tst", "3553534655", "user4.jpg", @TIME_NOW),
+(NULL, "Настя", "nastya12@email.tst", "4564765", "user5.jpg", @TIME_NOW),
+(NULL, "Юля", "yulja@email.tst", "567574757574", "user6.jpg", @TIME_NOW),
+(NULL, "Дима", "dima@email.tst", "965968958498", "user7.jpg", @TIME_NOW),
+(NULL, "Жорик", "zhora@email.tst", "6354635466", "user8.jpg", @TIME_NOW),
+(NULL, "Вадим", "vadim@email.tst", "3645635462", "user9.jpg", @TIME_NOW),
+(NULL, "Николай", "kolya@email.tst", "6546366", "user10.jpg", @TIME_NOW),
+(NULL, "Evgeniy", "evgen@email.tst", "3335355", "user11.jpg", @TIME_NOW);
+
+-- --------------------------------------------------------
+
+--
+-- Добавляем сообщения.
+--
+
+INSERT INTO `messages`
+(`id`, `from_id`, `to_id`, `message`, `created`)
+VALUES
+(NULL, 11, 1, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:00:00')),
+(NULL, 11, 2, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:05:00')),
+(NULL, 11, 3, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:10:00')),
+(NULL, 11, 4, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:15:00')),
+(NULL, 11, 5, "Привет! Как дела?;)", UNIX_TIMESTAMP('2020-02-17 01:20:00')),
+(NULL, 11, 6, "Привет! Как дела?;)", UNIX_TIMESTAMP('2020-02-17 01:25:00')),
+(NULL, 11, 7, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:30:03')),
+(NULL, 11, 8, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:35:00')),
+(NULL, 11, 9, "=)", UNIX_TIMESTAMP('2020-02-17 01:40:00')),
+(NULL, 11, 10, "Привет!;)", UNIX_TIMESTAMP('2020-02-17 01:45:10')),
+(NULL, 1, 11, "О! Привет!", UNIX_TIMESTAMP('2020-02-17 02:10:01')),
+(NULL, 1, 11, "Как дела?", UNIX_TIMESTAMP('2020-02-17 02:10:12')),
+(NULL, 11, 1, "Нормально.. Сам как?", UNIX_TIMESTAMP('2020-02-17 02:20:15')),
+(NULL, 1, 11, "Та я вот сижу и домашку пилю.", UNIX_TIMESTAMP('2020-02-17 02:22:00')),
+(NULL, 1, 11, "А еще и приболел немного.", UNIX_TIMESTAMP('2020-02-17 02:24:10')),
+(NULL, 11, 1, "Что нового?", UNIX_TIMESTAMP('2020-02-17 02:30:01')),
+(NULL, 1, 11, "Та сваливаю на выхах.", UNIX_TIMESTAMP('2020-02-17 02:31:22')),
+(NULL, 2, 11, "Дарова братан!!!", UNIX_TIMESTAMP('2020-02-17 03:11:01')),
+(NULL, 3, 11, "Ты кто такой?!", UNIX_TIMESTAMP('2020-02-17 04:12:11')),
+(NULL, 4, 11, "Ты где пропал?!", UNIX_TIMESTAMP('2020-02-17 05:33:30')),
+(NULL, 5, 11, "Все норм! Только приехала..", UNIX_TIMESTAMP('2020-02-17 05:45:12')),
+(NULL, 6, 11, "Все нормально =)", UNIX_TIMESTAMP('2020-02-17 06:12:01')),
+(NULL, 7, 11, "Слушай..тут такая тема надо встретится..", UNIX_TIMESTAMP('2020-02-17 06:15:11')),
+(NULL, 8, 11, "Приветы!", UNIX_TIMESTAMP('2020-02-17 06:20:01')),
+(NULL, 9, 11, ";)", UNIX_TIMESTAMP('2020-02-17 06:34:00')),
+(NULL, 10, 11, "Неть меня ))))))))", UNIX_TIMESTAMP('2020-02-17 06:40:00'));
---
+++ core/queries/install_tables.sql
@@ -0,0 +1,78 @@
+SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+SET time_zone = "+00:00";
+
+-- --------------------------------------------------------
+
+--
+-- База данных: `wchat`
+--
+
+CREATE DATABASE IF NOT EXISTS `wchat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `wchat`;
+
+-- --------------------------------------------------------
+
+--
+-- Пользователь БД.
+--
+
+CREATE USER IF NOT EXISTS 'wchat_user'@'%' IDENTIFIED BY 'password';
+GRANT USAGE ON *.* TO 'wchat_user'@'%' IDENTIFIED BY 'password'
+    REQUIRE NONE WITH
+        MAX_QUERIES_PER_HOUR 0
+        MAX_CONNECTIONS_PER_HOUR 0
+        MAX_UPDATES_PER_HOUR 0
+        MAX_USER_CONNECTIONS 0;
+
+GRANT ALL PRIVILEGES ON `wchat`.* TO 'wchat_user'@'%';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Индекс пользователя',
+    `username` varchar(64) NOT NULL DEFAULT '' COMMENT 'Логин пользователя',
+    `email` varchar(64) NOT NULL DEFAULT '' COMMENT 'Почта пользователя',
+    `phone` varchar(64) NOT NULL DEFAULT '' COMMENT 'Телефон пользователя',
+    `icon` varchar(64) NOT NULL DEFAULT '' COMMENT 'Иконка пользователя',
+    `created` int unsigned  NOT NULL DEFAULT 0 COMMENT 'Дата создания пользователя',
+
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`email`),
+    KEY (`username`)
+)
+ENGINE = MyISAM
+DEFAULT CHARSET = utf8
+COMMENT = 'Первичная информация о пользователе'
+AUTO_INCREMENT = 1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages`
+(
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Индекс пользователя',
+    `from_id` bigint(20) unsigned NOT NULL COMMENT 'Индекс пользователя отправителя',
+    `to_id` bigint(20) unsigned NOT NULL COMMENT 'Индекс пользователя получателя',
+    `message` text NOT NULL COMMENT 'Сообщение пользователя',
+    `created` int unsigned  NOT NULL DEFAULT 0 COMMENT 'Дата создания сообщения',
+
+    PRIMARY KEY (`id`),
+    KEY (`from_id`),
+    KEY (`to_id`)
+)
+ENGINE = MyISAM
+DEFAULT CHARSET = utf8
+COMMENT = 'Сообщения пользователей.'
+AUTO_INCREMENT = 1;
+
+-- --------------------------------------------------------
